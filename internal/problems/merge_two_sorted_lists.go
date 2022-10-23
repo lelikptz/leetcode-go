e9a4a858480cf203dde2f99c35c9a727bdce8e8b
aleksei.orlov <alexey@orlov.tech>
2022-10-23 13:49:10 +0600

    21. Merge Two Sorted Lists https://leetcode.com/problems/merge-two-sorted-lists/

---
+++ internal/problems/merge_two_sorted_lists.go
@@ -0,0 +1,32 @@
+package problems
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func MergeTwoLists(a *ListNode, b *ListNode) *ListNode {
+	head := &ListNode{
+		Val:  0,
+		Next: nil,
+	}
+
+	cur := head
+	for a != nil && b != nil {
+		if a.Val > b.Val {
+			cur.Next = b
+			b = b.Next
+		} else {
+			cur.Next = a
+			a = a.Next
+		}
+		cur = cur.Next
+	}
+	if a != nil {
+		cur.Next = a
+	} else {
+		cur.Next = b
+	}
+
+	return head.Next
+}
---
+++ internal/problems/merge_two_sorted_lists_test.go
@@ -0,0 +1,60 @@
+package problems
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMergeTwoLists(t *testing.T) {
+	type args struct {
+		a *ListNode
+		b *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "First",
+			args: args{
+				a: createList(1, 2, 4),
+				b: createList(1, 3, 4),
+			},
+			want: createList(1, 1, 2, 3, 4, 4),
+		},
+		{
+			name: "Second",
+			args: args{
+				a: createList(1, 2, 3, 4),
+				b: createList(5, 6, 7),
+			},
+			want: createList(1, 2, 3, 4, 5, 6, 7),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MergeTwoLists(tt.args.a, tt.args.b); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MergeTwoLists() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func createList(elements ...int) *ListNode {
+	slice := make([]ListNode, len(elements), len(elements))
+
+	for i, el := range elements {
+		slice[i] = ListNode{
+			Val: el,
+		}
+	}
+
+	for index := range slice {
+		if len(slice) > index+1 {
+			slice[index].Next = &slice[index+1]
+		}
+	}
+
+	return &slice[0]
+}
