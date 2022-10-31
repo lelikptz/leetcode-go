92c96ba601dc095f108c57ddb73611b193c098b8
Alexey Orlov <orlov@proitr.ru>
2022-10-31 20:39:39 +0600

    70. Climbing Stairs https://leetcode.com/problems/climbing-stairs/

---
+++ internal/problems/climbing_stairs.go
@@ -0,0 +1,12 @@
+package problems
+
+func ClimbStairs(n int) int {
+	fib := make(map[int]int)
+	fib[1] = 1
+	fib[2] = 2
+	for i := 3; i <= 45; i++ {
+		fib[i] = fib[i-1] + fib[i-2]
+	}
+
+	return fib[n]
+}
