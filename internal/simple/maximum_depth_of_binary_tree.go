ec7ec53d4c0308af1f58c65ab67058a0570f252b
Alexey Orlov <orlov@proitr.ru>
2022-11-11 12:16:51 +0600

    104. Maximum Depth of Binary Tree https://leetcode.com/problems/maximum-depth-of-binary-tree/

---
+++ internal/simple/maximum_depth_of_binary_tree.go
@@ -0,0 +1,17 @@
+package simple
+
+import "github.com/lelikptz/leetcode-go/internal/structure"
+
+func MaxDepth(root *structure.TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	left := MaxDepth(root.Left)
+	right := MaxDepth(root.Right)
+	if left > right {
+		return left + 1
+	}
+
+	return right + 1
+}
