aa49e05dfee983511f16002f573daa17564f83c4
lelikptz <alexey@orlov.tech>
2022-11-23 13:04:48 +0600

    119. Pascal's Triangle II https://leetcode.com/problems/pascals-triangle-ii/

---
+++ internal/simple/pascal_triangle_2.go
@@ -0,0 +1,20 @@
+package simple
+
+func GetRow(rowIndex int) []int {
+	if rowIndex == 0 {
+		return []int{1}
+	}
+	if rowIndex == 1 {
+		return []int{1, 1}
+	}
+
+	prev := GetRow(rowIndex - 1)
+	next := []int{1}
+
+	for i := 1; i < len(prev); i++ {
+		next = append(next, prev[i]+prev[i-1])
+	}
+	next = append(next, 1)
+
+	return next
+}
