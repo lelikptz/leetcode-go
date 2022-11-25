1f72196a34294140c90b8e7dc696c388ab152afe
lelikptz <alexey@orlov.tech>
2022-11-25 12:03:39 +0600

    121. Best Time to Buy and Sell Stock https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

--- internal/medium/word_search_test.go
+++ internal/medium/word_search_test.go
@@ -37,6 +37,7 @@ func TestExist(t *testing.T) {
 			want: false,
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Exist(tt.args.board, tt.args.word); got != tt.want {
