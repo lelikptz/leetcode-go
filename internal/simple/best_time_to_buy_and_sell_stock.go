06e76e2567270bd315df1bd90e442e0f9478728d
lelikptz <alexey@orlov.tech>
2022-11-25 21:49:24 +0600

    121. Best Time to Buy and Sell Stock https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

--- internal/simple/best_time_to_buy_and_sell_stock.go
+++ internal/simple/best_time_to_buy_and_sell_stock.go
@@ -1,52 +1,16 @@
 package simple
 
 func MaxProfit(prices []int) int {
-	prepared := preparePricesList(prices)
-
-	return calculateMaxProfit(prepared, 0)
-}
-
-func preparePricesList(prices []int) []int {
-	prepared := make([]int, 0, len(prices))
-
-	for i := 0; i < len(prices); i++ {
-		if i == 0 || i == len(prices)-1 || (!(prices[i] > prices[i-1] && prices[i] < prices[i+1]) && !(prices[i] <= prices[i-1] && prices[i] >= prices[i+1])) {
-			prepared = append(prepared, prices[i])
-		}
-	}
-
-	return prepared
-}
-
-func calculateMaxProfit(prices []int, index int) int {
-	if index >= len(prices) {
-		return 0
-	}
-
-	max := prices[index]
-	maxIndex := index
-	for i := index; i < len(prices); i++ {
-		if prices[i] > max {
-			max = prices[i]
-			maxIndex = i
-		}
-	}
-
-	min := prices[index]
-	for i := index; i < maxIndex; i++ {
-		if prices[i] < min {
+	min := prices[0]
+	max := 0
+	for i := 1; i < len(prices); i++ {
+		if min > prices[i] {
 			min = prices[i]
 		}
+		if prices[i]-min > max {
+			max = prices[i] - min
+		}
 	}
 
-	if maxIndex == index {
-		maxIndex = index + 1
-	}
-
-	x := calculateMaxProfit(prices, maxIndex)
-	if max-min > x {
-		return max - min
-	} else {
-		return x
-	}
+	return max
 }
