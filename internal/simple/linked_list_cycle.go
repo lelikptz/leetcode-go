70db806238e06d912e3c222a8c6dceb0f21a61ef
lelikptz <alexey@orlov.tech>
2022-11-29 12:38:32 +0600

    141. Linked List Cycle https://leetcode.com/problems/linked-list-cycle/

---
+++ internal/simple/linked_list_cycle.go
@@ -0,0 +1,21 @@
+package simple
+
+import (
+	"github.com/lelikptz/leetcode-go/internal/structure"
+	"math"
+)
+
+func HasCycle(head *structure.ListNode) bool {
+	max := int(math.Pow(10.0, 5.0))
+	cur := head
+	for {
+		if cur == nil {
+			return false
+		}
+		if cur.Val == max {
+			return true
+		}
+		cur.Val = max
+		cur = cur.Next
+	}
+}
